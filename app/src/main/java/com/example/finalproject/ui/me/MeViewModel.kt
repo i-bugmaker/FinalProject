beec189590b4f6d91da75f9176b254a4c8c785db
mh <291147521@qq.com>
2022-04-29 15:36:54 +0800

    底部导航栏的创建及其矢量图片

--- app/src/main/java/com/example/finalproject/ui/me/MeViewModel.kt
+++ app/src/main/java/com/example/finalproject/ui/me/MeViewModel.kt
@@ -4,10 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class InfoViewModel : ViewModel() {
-
+class MeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is me Fragment"
+        value = "This is info Fragment"
     }
     val text: LiveData<String> = _text
 }
