4a19dba98ca2f19300d25f953a02eb2859955f44
mh <291147521@qq.com>
2022-04-29 15:09:46 +0800

    底部导航栏的创建及其矢量图片

---
+++ app/src/main/java/com/example/finalproject/ui/forum/ForumFragment.kt
@@ -0,0 +1,42 @@
+package com.example.finalproject.ui.forum
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.finalproject.databinding.FragmentForumBinding
+
+class ForumFragment : Fragment() {
+
+    private var _binding: FragmentForumBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val notificationsViewModel =
+            ViewModelProvider(this).get(ForumViewModel::class.java)
+
+        _binding = FragmentForumBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textForum
+        notificationsViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
