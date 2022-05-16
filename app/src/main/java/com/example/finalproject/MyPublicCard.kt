97f7ed81439c6a133014d859e9b26e1010c1bfc9
i-bugmaker <291147521@qq.com>
2022-05-16 16:17:58 +0800

    我的发布实现成功(补回忘添加的文件)

---
+++ app/src/main/java/com/example/finalproject/MyPublicCard.kt
@@ -0,0 +1,13 @@
+package com.example.finalproject
+
+import android.graphics.Bitmap
+
+class MyPublicCard(
+    val nickname: String,
+    val breed: String,
+    val age: String,
+    val sex: String,
+    val image: Bitmap,
+    val date: String,
+    val description: String
+)
